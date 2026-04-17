76d2e0120feacdbf8531b5f55cd64e2dff5d3f9a
João Gabriel Silva Barros <guieste2@gmail.com>
2026-04-17 17:21:28 -0300

    adicionando classe funcionario

---
+++ src/br/com/FolhaDePagamento/Model/Funcionario.java
@@ -0,0 +1,38 @@
+package br.com.FolhaDePagamento.Model;
+
+import java.time.LocalDate;
+
+public final class Funcionario extends Pessoa {
+    private double salarioBruto;
+    private Departamento departamento;
+
+    public Funcionario(String cpf, String nome, LocalDate nascimento, double salarioBruto, Departamento departamento) {
+        super(cpf, nome, nascimento);
+        this.salarioBruto = salarioBruto;
+        this.departamento = departamento;
+    }
+
+    public double getSalarioBruto() {
+        return salarioBruto;
+    }
+
+    public void setSalarioBruto(double salarioBruto) {
+        this.salarioBruto = salarioBruto;
+    }
+
+    public Departamento getDepartamento() {
+        return departamento;
+    }
+
+    public void setDepartamento(Departamento departamento) {
+        this.departamento = departamento;
+    }
+
+    @Override
+    public String toString() {
+        return "Funcionario{" +
+                "departamento=" + departamento +
+                ", salarioBruto=" + salarioBruto +
+                '}';
+    }
+}
