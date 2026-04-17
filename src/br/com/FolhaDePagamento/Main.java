4bdf0cc0ff358892ed37405d253d7ddbf74c723d
marceloribeiro-serratec <marcelo.ribeiro@residente.serratec.org>
2026-04-17 10:28:36 -0300

    finalizacao do sistema

--- src/br/com/FolhaDePagamento/Main.java
+++ src/br/com/FolhaDePagamento/Main.java
@@ -1,9 +1,24 @@
 package br.com.FolhaDePagamento;
 
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner sc = new Scanner(System.in);
+        String opcaoDeSaida = "";
+
+        do{
+
+            System.out.println("Deseja continuar(S/N):");
+            opcaoDeSaida = sc.nextLine();
+        }
+        while(!opcaoDeSaida.equalsIgnoreCase("S"));
+
+        sc.close();
+        System.out.println("====================");
+        System.out.println("Programa finalizado com sucesso.");
+        System.out.println("====================");
     }
 }
