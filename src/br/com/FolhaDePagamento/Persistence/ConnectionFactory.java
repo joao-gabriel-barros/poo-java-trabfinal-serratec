aa472210db933884d4b39bb0b5a182179df7a75d
marceloribeiro-serratec <marcelo.ribeiro@residente.serratec.org>
2026-04-17 16:26:37 -0300

    mudanca de banco de dados

--- src/br/com/FolhaDePagamento/Persistence/ConnectionFactory.java
+++ src/br/com/FolhaDePagamento/Persistence/ConnectionFactory.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionFactory {
-    final String url = "jdbc:postgresql://localhost:5432/folha-pagamento";
+    final String url = "jdbc:postgresql://localhost:5432/folhapagamento";
     final String usuario = "postgres";
     final String senha = "postgres";
     private Connection connection;
@@ -16,7 +16,7 @@ public class ConnectionFactory {
         try {
             connection = DriverManager.getConnection(url, usuario, senha);
             if (connection != null) {
-                System.out.println("\nConectado com sucesso!");
+                System.out.println("\nConectado com sucesso ao banco de dados.....!");
             } else {
                 System.out.println("Erro nos dados da conexão!");
             }
