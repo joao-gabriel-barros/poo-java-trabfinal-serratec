5ad1371f2ad5c46073e043eb4b476c23c38ac29f
João Gabriel Silva Barros <guieste2@gmail.com>
2026-04-17 16:48:40 -0300

    adicionando classe pessoa

---
+++ src/br/com/FolhaDePagamento/Model/Pessoa.java
@@ -0,0 +1,48 @@
+package br.com.FolhaDePagamento.Model;
+
+import java.time.LocalDate;
+
+public sealed abstract class Pessoa permits Funcionario, Dependente {
+    protected String cpf;
+    protected String nome;
+    protected LocalDate nascimento;
+
+    public Pessoa(String cpf, String nome, LocalDate nascimento) {
+        this.cpf = cpf;
+        this.nome = nome;
+        this.nascimento = nascimento;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public LocalDate getNascimento() {
+        return nascimento;
+    }
+
+    public void setNascimento(LocalDate nascimento) {
+        this.nascimento = nascimento;
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa{" +
+                "cpf='" + cpf + '\'' +
+                ", nome='" + nome + '\'' +
+                ", nascimento=" + nascimento +
+                '}';
+    }
+}
