2edc874e11303735b0d7b77343b5abb0e2f624ab
marceloribeiro-serratec <marcelo.ribeiro@residente.serratec.org>
2026-04-18 09:35:37 -0300

    adicionando a classe FolhaDePagamento com atributos e métodos necessários

---
+++ src/br/com/FolhaDePagamento/Model/FolhaDePagamento.java
@@ -0,0 +1,86 @@
+package br.com.FolhaDePagamento.Model;
+
+//todo - Folha de Pagamento: Tabela folha_pagamento (ID, CPF_Funcionario, Data, INSS, IR, Liquido).
+
+import java.time.LocalDate;
+
+public class FolhaDePagamento {
+    private int id;
+    private String cpf_funcionario;
+    private LocalDate data;
+    private double inss;
+    private double ir;
+    private double liquido;
+
+    //construtor
+    public FolhaDePagamento(int id, String cpf_funcionario, LocalDate data, double inss, double ir, double liquido) {
+        this.id = id;
+        this.cpf_funcionario = cpf_funcionario;
+        this.data = data;
+        this.inss = inss;
+        this.ir = ir;
+        this.liquido = liquido;
+    }
+
+    // getters e setters
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCpf_funcionario() {
+        return cpf_funcionario;
+    }
+
+    public void setCpf_funcionario(String cpf_funcionario) {
+        this.cpf_funcionario = cpf_funcionario;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    public double getInss() {
+        return inss;
+    }
+
+    public void setInss(double inss) {
+        this.inss = inss;
+    }
+
+    public double getIr() {
+        return ir;
+    }
+
+    public void setIr(double ir) {
+        this.ir = ir;
+    }
+
+    public double getLiquido() {
+        return liquido;
+    }
+
+    public void setLiquido(double liquido) {
+        this.liquido = liquido;
+    }
+
+    @Override
+    public String toString() {
+        return "FolhaDePagamento{ " +
+                "id=" + id +
+                ", cpf_funcionario='" + cpf_funcionario + '\'' +
+                ", data=" + data +
+                ", inss=" + inss +
+                ", ir=" + ir +
+                ", liquido=" + liquido +
+                '}';
+    }
+}
