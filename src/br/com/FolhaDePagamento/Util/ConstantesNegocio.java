41b30780c88fb627ec1eec7f7f04845ca20d2f5e
marceloribeiro-serratec <marcelo.ribeiro@residente.serratec.org>
2026-04-18 08:57:07 -0300

    removendo retorno de dependentes na classe CsvFileReader

---
+++ src/br/com/FolhaDePagamento/Util/ConstantesNegocio.java
@@ -0,0 +1,9 @@
+package br.com.FolhaDePagamento.Util;
+
+public class ConstantesNegocio {
+    public static final double SALARIO_MINIMO = 1412.00;
+    public static final double VALOR_DEPENDENTE_IR = 189.59;
+    public static final double TETO_INSS = 951.62;
+
+    private ConstantesNegocio() {}
+}
