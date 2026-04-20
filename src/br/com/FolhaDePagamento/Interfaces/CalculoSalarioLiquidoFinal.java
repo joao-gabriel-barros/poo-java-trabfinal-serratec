2007f8bca29720170c39cf4c199e28cab6738ae3
pedromartins01 <pedropintomartinsdesouza@gmail.com>
2026-04-20 15:59:07 -0300

    Corrigindo interface salário líquido final

--- src/br/com/FolhaDePagamento/Interfaces/CalculoSalarioLiquidoFinal.java
+++ src/br/com/FolhaDePagamento/Interfaces/CalculoSalarioLiquidoFinal.java
@@ -1,5 +1,6 @@
 package br.com.FolhaDePagamento.Interfaces;
 
-public interface CalculoSalarioLiquidoFinal {
-
+	public interface CalculoSalarioLiquidoFinal {
+	    
+		double calcularSalario(double salarioBruto, double inss, double irpf);
 }
